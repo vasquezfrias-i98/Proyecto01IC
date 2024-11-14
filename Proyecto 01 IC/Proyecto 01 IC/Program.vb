4b02cd305866f267a9242d8a6d36185e5acac2ed
Ilter Clevert <vasfric98@gmail.com>
2024-11-14 12:20:27 -0500

    Primera actualizacion del Proyecto

---
+++ Proyecto 01 IC/Proyecto 01 IC/Program.vb
@@ -0,0 +1,9 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+
+        Console.WriteLine("Hello World!")
+        Console.WriteLine("Ilter Clevert Vasquez Frias")
+    End Sub
+End Module
